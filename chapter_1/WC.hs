393a38c4f9a70be8eec81dda02340f2b4953e6f4
l-shane-l <shane.hamilton3@mail.dcu.ie>
2021-10-06 18:24:07 +0200

    question 4

--- chapter_1/WC.hs
+++ chapter_1/WC.hs
@@ -1,5 +1,5 @@
 main = interact wordCount
-    where wordCount input = show (length (words input)) ++ "\n"
+    where wordCount input = show (length input) ++ "\n"
 
 -- wrote this with vim, using tab causes compile error, I think this can be fixed in the vim setting, but need to investigate
 --runghc WC < text.txt

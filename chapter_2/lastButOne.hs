b1c16ee8f6b0e66d805b9d41b7a5dc1d66be87f3
l-shane-l <shane.hamilton3@mail.dcu.ie>
2021-10-12 01:31:12 +0200

    chapter two finished

--- chapter_2/lastButOne.hs
+++ chapter_2/lastButOne.hs
@@ -1,4 +1,13 @@
 
 lastButOne  :: [a] -> a
+-- type def
+lastButOne [] = error "empty list"
+-- handle empty list
+lastButOne [x] = error "list too short"
+-- handle list too short
+lastButOne [x, _] = x
+-- if tuple return first value, this is the desired outcome
+lastButOne (x: xs) =  lastButOne xs  
+-- recursively call function on tail of list until it becomes a tuple.
 
 
